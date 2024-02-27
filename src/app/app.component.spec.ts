b307cc0824ccb145a4ad62330096f2307d800231
Neo Shi <shiwenxuan2003@gmail.com>
2024-02-28 00:16:44 +0800

    init for computer networking course

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-starter app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('A Top-Down Approach');
   });
 });
